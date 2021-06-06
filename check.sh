081823c405c6d78d5aa1132af90245f7220bf01a
Aravind Easwar <aeaswar81@gmail.com>
2021-06-07 02:01:50 +0530

    second dose option added

--- check.sh
+++ check.sh
@@ -10,19 +10,18 @@ check_vac () {
 #get the data
 a=$(curl -sX GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=$1&date=$2" -H  "accept: application/json" -H  "Accept-Language: hi_IN")
 #get the available doses
-readarray -t barray < <(jq -e '.sessions[].available_capacity_dose1'<<<"$a")
+readarray -t barray < <(jq -e ".sessions[].available_capacity_dose$4"<<<"$a")
 readarray -t agearray < <(jq -e '.sessions[].min_age_limit'<<<"$a")
-
 #if its empty then no centre is open
 if [ -z "$barray" ];
 then
 #if running only once print output 
-	if [ "$4" = "1" ]; 
+	if [ "$5" = "1" ]; 
 	then
 		echo "slot not open"
 	fi
 	 
-#depending on the number of centres for that pincode b can be string separated by spaces , split string into array
+#check b with corresponding age
 else 
 	centres=0
 	for index in "${!barray[@]}"
@@ -40,7 +39,7 @@ else
 	fi
 	done
 	
-	if [ "$4" = "1" ];
+	if [ "$5" = "1" ];
 	then
 		echo $centres "centres open for " $3 " and above"
 		echo "0 doses found"
@@ -48,17 +47,17 @@ else
 fi
 }
 
-echo "Checks only for 1st dose"
-if [ "$4" = "1" ];
+echo "Checking for $4 dose"
+if [ "$5" = "1" ];
 then
 echo "checking only once"
 #arguments 1 and 2 are pincode and date respectively , 3 is once 
-check_vac $1 $2 $3 $4
+check_vac $1 $2 $3 $4 $5
 else
 echo "looping"
 	while true
 	do
-		check_vac $1 $2 $3
+		check_vac $1 $2 $3 $4
 		sleep 1
 	done 
 	
