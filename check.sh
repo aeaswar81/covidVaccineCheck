bff83e7b4b35f5b90c2aa83bf011b33c285c1c33
Aravind Easwar <aeaswar81@gmail.com>
2021-06-07 00:27:15 +0530

    age added

--- check.sh
+++ check.sh
@@ -11,20 +11,26 @@ check_vac () {
 a=$(curl -sX GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=$1&date=$2" -H  "accept: application/json" -H  "Accept-Language: hi_IN")
 #get the available doses
 b=$(jq -e '.sessions[].available_capacity_dose1'<<<"$a")
+age=$(jq -e '.sessions[].min_age_limit'<<<"$a")
+
 #if its empty then no centre is open
 if [ -z "$b" ];
 then
 #if running only once print output 
-	if [ "$3" = "1" ]; 
+	if [ "$4" = "1" ]; 
 	then
 		echo "slot not open"
 	fi
 	 
-#depending on the number of centres for that pincode b can be string separated by spaces 
+#depending on the number of centres for that pincode b can be string separated by spaces , split string into array
 else 
-	for i in $b
+	IFS=' ' read -r -a barray <<< "$b"
+	IFS=' ' read -r -a garray <<< "$age"
+
+	for index in "${!barray[@]}"
 	do
-	if [ "$i" != "0" ];
+	if [ "${garray[index]}" -le "$3" ];then
+	if [ "${barray[index]}" != "0" ];
 	then
 		echo "Doses found"
 		echo "Book now :D "
@@ -32,27 +38,28 @@ else
 
 		return 1
 	fi
+	fi
 	done
 	
-	if [ "$3" = "1" ];
+	if [ "$4" = "1" ];
 	then
-		echo $((${#b}-${#b}/2)) "centres open"
+		echo ${#barray[@]} "centres open"
 		echo "0 doses found"
 	fi
 fi
 }
 
 
-if [ "$3" = "1" ];
+if [ "$4" = "1" ];
 then
 echo "checking only once"
 #arguments 1 and 2 are pincode and date respectively , 3 is once 
-check_vac $1 $2 $3
+check_vac $1 $2 $3 $4
 else
 echo "looping"
 	while true
 	do
-		check_vac $1 $2
+		check_vac $1 $2 $3
 		sleep 1
 	done 
 	
